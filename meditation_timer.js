c7e30e28cf623cbe6a0e548aa8693afc806e1958
Ken P <ken@kencodes.com>
2015-03-17 20:11:20 -0500

    Initial Commit

---
+++ meditation_timer.js
@@ -0,0 +1,17 @@
+var meditation = function(){
+
+  var rawTime = document.getElementById("totaltime").val();
+
+  var convertTimeToMs = function(rawTime){
+
+  };
+
+  var countdown = function (totalTime){
+    for()
+  };
+
+};
+
+$("#button").click(function{
+  meditation();
+});
